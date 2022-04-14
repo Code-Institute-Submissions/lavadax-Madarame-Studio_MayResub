fcea65ec84820ef527095c350a82dcdd15409414
Kevin Schepers <gamer.mmo@gmail.com>
2022-04-14 17:32:26 +0000

    Add product details view

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+"""
+Home page urls for the madarame_studio project
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.all_products, name="products"),
+    path('<product_id>', views.product_detail, name="product_detail"),
+]
