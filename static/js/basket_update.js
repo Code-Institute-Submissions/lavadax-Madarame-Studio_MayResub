ae0ec0cd4c5f0076dc552af8b7c255fc6be169ce
Kevin Schepers <kevinschepers@hotmail.com>
2022-05-07 21:11:07 +0000

    Add csrftoken in static js file

--- static/js/basket_update.js
+++ static/js/basket_update.js
@@ -6,7 +6,7 @@ $(".update-link").click(function(e) {
 
 // Remove item and reload on click
 $(".remove-item").click(function(e) {
-    let csrfToken = "{{ csrf_token }}";
+    let csrfToken = getCookie("csrftoken")
     let itemId = $(this).attr("id").split("remove_")[1];
     let size = $(this).data("product_size");
     let url = `/basket/remove/${itemId}/`;
@@ -17,3 +17,19 @@ $(".remove-item").click(function(e) {
             location.reload();
         });
 });
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
